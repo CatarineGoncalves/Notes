9d6d069b6df514760889f6033d966e59a4da9a31
Catarine Gonçalves <Catarine>
2024-02-10 13:39:29 -0300

    terminado

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,75 @@
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlwexpert.svg";
 import { NewCard } from "./components/new-note-cards";
 import { NoteCard } from "./components/note-cards";
 
 
-
+interface Note {
+    id: string
+    date: Date
+    content: string
+}
 
 export function App() {
+
+    const [search, setSearch] = useState('')
+
+    const [notes, setNotes] =  useState<Note[]>(() => {
+        const notesOnStorage = localStorage.getItem('notes')
+        
+        if (notesOnStorage) {
+            return JSON.parse(notesOnStorage)
+        }
+
+        return []
+    })
+    
+
+    function onNoteCreated(content: string) {
+        const newNote = { 
+            id: crypto.randomUUID(),
+            date: new Date(),
+            content,
+        }
+
+        const notesArray = [newNote, ...notes]
+
+        setNotes(notesArray)
+
+        localStorage.setItem('notes', JSON.stringify( notesArray ))
+
+
+        // JSON = Javscript object notation
+
+    }
+
+    function onNoteDeleted(id: string) {
+        const notesArray = notes.filter(note => { 
+            return note.id !== id
+        })
+
+        setNotes(notesArray)
+
+        localStorage.setItem('notes', JSON.stringify(notesArray)
+    )}
+
+
+    function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+        
+        const query = event.target.value 
+
+        setSearch(query)
+    }
+    
+
+    // ao adicionar o lowcase ficara mais sensivel para achar independente do maisuculo e minusculo 
+    const filteredNotes = search !== '' 
+        ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+        : notes
+
+
     return (
-        <div className="mx-auto max-w-6xl my-12 space-y-6">
+        <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
             <img src={logo} alt="logo da nlw"/>
 
             <form className="w-full">
@@ -15,22 +77,22 @@ export function App() {
                     type="text" 
                     placeholder="Busque em suas notas"
                     className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+                    onChange={handleSearch}
                 />
             </form>
        
             <div className="h-px bg-slate-700"/>
             
-            <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+            <div className="grid grid-cols-1 md:gril-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
                
-                <NewCard/>
+                <NewCard onNoteCreated={onNoteCreated}/>
 
-                <NoteCard note={{ 
-                
-                    date: new Date(),
-                    content: 'hello wolrd' 
-                    
-                }}/>
 
+                {filteredNotes  .map(note => { 
+                    return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
+                }
+
+                )}
             </div>
 
 
--- src/components/new-note-cards.tsx
+++ src/components/new-note-cards.tsx
@@ -3,9 +3,20 @@ import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from "react"
 import { toast } from 'sonner'
 
-export function NewCard() {
+
+
+
+
+interface NewCardProps { 
+    onNoteCreated: (content: string) => void
+}
+
+let speechRecognition: SpeechRecognition | null = null
+
+export function NewCard({ onNoteCreated }: NewCardProps) {
     
     const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true) 
+    const [isRecording, setIsRecording] = useState(false)
     const [content, setContent] = useState('')
 
     function handleStartEditor() {
@@ -23,9 +34,64 @@ export function NewCard() {
     function handleSaveNote(event: FormEvent) {
         event.preventDefault()
 
-        console.log(content)
+        // para evitar que salve notas vazias
+        if ( content === '') {
+            return
+        }
+
+
+        onNoteCreated(content)
+
+        setContent('')
+        setShouldShowOnboarding (true)
 
         toast.success('nota criada com sucesso')
+    }
+
+    function handlerStartRecording() {
+        
+
+        const isSpeechRecognitionAPIAvailable = 'SpeechRecognition' in window || 'webkitSpeechRecognition' in window
+        
+        if (!isSpeechRecognitionAPIAvailable) {
+            alert('Infelizmente teu navegador não suporta a API de Gravação')
+            return
+        }
+
+        setIsRecording(true)
+        setShouldShowOnboarding (false)
+
+
+        const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
+    
+        speechRecognition = new SpeechRecognitionAPI
+
+        speechRecognition.lang = 'pt-BR'
+        speechRecognition.continuous = true
+        speechRecognition.maxAlternatives = 1
+        speechRecognition.interimResults = true
+
+        speechRecognition.onresult = (event) => {
+           const transcription = Array.from(event.results).reduce((text, result) => {
+                return text.concat(result[0].transcript)
+           }, '')
+
+           setContent(transcription)
+        }
+
+        speechRecognition.onerror = (event) => {
+            console.error(event)
+        }
+
+        speechRecognition.start()
+    }
+
+    function handleStopRecording() {
+        setIsRecording(false)
+
+        if (speechRecognition !== null) {
+            speechRecognition.stop()
+        }
     }
 
     return (
@@ -50,14 +116,14 @@ export function NewCard() {
            
                 <Dialog.Overlay className="inset-0 fixed bg-black/50 ">
                     
-                    <Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+                    <Dialog.Content className="overflow-hidden fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:w-full h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
                        
                        
                         <Dialog.Close className=" absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 rounded-bl-lg hover:text-slate-100 ">
                             <X className="size-5"/>
                         </Dialog.Close>
                         
-                        <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+                        <form className="flex-1 flex flex-col">
                             <div className="flex flex-1 flex-col gap-3 p-5 ">
                                 <span className="text-small font-medium text-slate-200 "> 
                                         Adicionar Nota
@@ -65,7 +131,7 @@ export function NewCard() {
 
                                 {shouldShowOnboarding ? (
                                     <p className="text-sm leading-6 text-slate-400">
-                                        Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-medium text-lime-400 hover:underline">utilize apenas texto</button>.
+                                        Comece <button type="button" onClick={handlerStartRecording} className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button type="button" onClick={handleStartEditor} className="font-medium text-lime-400 hover:underline">utilize apenas texto</button>.
                                     </p> 
                                 ) : (
 
@@ -73,6 +139,7 @@ export function NewCard() {
                                     autoFocus
                                     className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                                     onChange={handleContentChanged}
+                                    value={content}
                                 />
 
                                 ) }  
@@ -80,9 +147,25 @@ export function NewCard() {
                                 
                             </div>
 
-                            <button type="submit" className="w-full bg-lime-400 py-4 text-center text-sm text-slate-950 outline-none hover:bg-lime-500">
-                                Salvar Nota
-                            </button>
+                                    {isRecording ? ( 
+                                        <button 
+                                            onClick={handleStopRecording}
+                                            type="button" 
+                                            className="w-full flex items-center justify-center gap-3 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none hover:text-slate-100">
+                                                <div className="size-3 rounded-full bg-red-500 animate-pulse"/>
+                                                Gravando! (clique para interromper)
+                                        </button>
+                                    ) : ( 
+                                        <button     
+                                            onClick={handleSaveNote}
+                                            type="button"
+                                            className="w-full bg-lime-400 py-4 text-center text-sm text-slate-950 outline-none hover:bg-lime-500"
+                                            >
+                                                Salvar Nota
+                                            </button>
+                                    )}
+
+
                         </form>
                     </Dialog.Content>
             
